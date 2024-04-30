c344247e99e87e073177c746bece8a25df256437
10041108 <aksmfdyfl111@gmail.com>
2024-04-30 09:30:08 +0900

    main

---
+++ chap01-junit5-annotation/src/test/java/com/ohgiraffers/section05/custom/ProductionTest.java
@@ -0,0 +1,18 @@
+package com.ohgiraffers.section05.custom;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@DisplayName("제품_테스트")
+@Tag("production")
+@Test
+public @interface ProductionTest {
+}
